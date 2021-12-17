0dbc0193603ea9e76638aea63c5da2af089def07
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-12-17 19:11:14 -0300

    Create ListaSetorResponse.cs

---
+++ ClientHttp/ClientHttp.NET/Response/Setor/ListaSetorResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientHttp.NET.Response.Setor
+{
+    public class ListaSetorResponse
+    {
+        public List<SetorResponse> Setores { get; set; } = new();
+    }
+}
