7a617b4d6cc7c394382071b66143927af2dedf2b
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-12-17 19:11:09 -0300

    Create CLientHttpController

---
+++ ClientHttp/ClientHttp.NET/Controllers/ClientHttpController.cs
@@ -0,0 +1,40 @@
+﻿using ClientHttp.NET.Response.Setor;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace ClientHttp.NET.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientHttpController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+
+        public ClientHttpController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            var url = "https://localhost:44358/api/Setor";
+
+            var ret = await _httpClient.GetAsync(url);
+
+            if (ret.StatusCode != HttpStatusCode.OK)
+                throw new InvalidOperationException("Código de Status Http 200 esperado!");
+
+            var result = await ret.Content.ReadFromJsonAsync<ListaSetorResponse>();
+
+            return Ok(result);
+        }
+    }
+}
--- ClientHttp/ClientHttp.NET/Startup.cs
+++ ClientHttp/ClientHttp.NET/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace ClientHttp.NET
@@ -32,6 +33,8 @@ namespace ClientHttp.NET
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ClientHttp.NET", Version = "v1" });
             });
+            // INJEÇÃO DE DEPEDENCIA
+            services.AddScoped<HttpClient>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
