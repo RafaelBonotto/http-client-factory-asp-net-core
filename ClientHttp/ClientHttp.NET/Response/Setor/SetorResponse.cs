3b14b24e1fdebbbaa531362c3ab7037ad5d5e387
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-12-17 19:11:18 -0300

    Create SetorResponse.cs

---
+++ ClientHttp/ClientHttp.NET/Response/Setor/SetorResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientHttp.NET.Response.Setor
+{
+    public class SetorResponse
+    {
+        public long Id { get; set; }
+        public string Descricao { get; set; }
+        public sbyte Ativo { get; set; }
+        public DateTime Data_criacao { get; set; }
+        public DateTime Data_alteracao { get; set; }
+    }
+}
