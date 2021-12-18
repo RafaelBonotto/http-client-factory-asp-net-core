71f49ce23feed48ce6792f075fb69b2c4022391d
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-12-18 00:56:21 -0300

    Create Setor.cs

---
+++ ClientHttp/ClientHttp.NET/Entities/Setor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClientHttp.NET.Entities
+{
+    public class Setor
+    {
+        public long Id { get; set; }
+        public string Descricao { get; set; }
+        public sbyte Ativo { get; set; }
+        public DateTime Data_criacao { get; set; }
+        public DateTime Data_alteracao { get; set; }
+    }
+}
