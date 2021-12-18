65d778c18d00ba55cb02cb25bb7d12f4bb496aaa
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-12-18 00:56:13 -0300

    Create ApiConfig.cs

---
+++ ClientHttp/ClientHttp.NET/Configurations/ApiConfig.cs
@@ -0,0 +1,21 @@
+﻿namespace ClientHttp.NET.Configurations
+{
+    public static class ApiConfig
+    {
+        public static string UrlBase  { get; set;}
+
+        public static Urls _Urls = new();
+
+        public class Urls
+        {
+            public Setor Setor { get; set; } = new();
+        }
+
+        public class Setor
+        {
+            public string Get { get; set; }
+            public string GetById { get; set; }
+            public string Post { get; set; }
+        }
+    }
+}
