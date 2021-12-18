39643e0e3678f7aafdc8f6a07adea28168370fe2
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-12-18 00:56:26 -0300

    Create SetorRequest.cs

---
+++ ClientHttp/ClientHttp.NET/Request/Setor/SetorRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClientHttp.NET.Request.Setor
+{
+    public class SetorRequest
+    {
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "Este campo deve conter entre 3 e 40 caracteres")]
+        public string Descricao { get; set; }
+    }
+}
