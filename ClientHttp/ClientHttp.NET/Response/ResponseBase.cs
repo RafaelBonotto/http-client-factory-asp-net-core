26de5efa556b8e43659c0d1d91f0e0a18f6972cb
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-12-18 00:56:31 -0300

    Create ResponseBase.cs

---
+++ ClientHttp/ClientHttp.NET/Response/ResponseBase.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace ClientHttp.NET.Response
+{
+    public class ResponseBase<T>
+    {
+        public T Data { get; set; }
+        public List<string> Errors { get; set; } = new();
+    }
+}
+
