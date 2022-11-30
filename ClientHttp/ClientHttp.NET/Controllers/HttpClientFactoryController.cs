af97bd59d3084ac5788bb9087c06decd8ad13921
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-11-30 16:18:56 -0300

    Get com httpClientFactory

--- ClientHttp/ClientHttp.NET/Controllers/HttpClientFactoryController.cs
+++ ClientHttp/ClientHttp.NET/Controllers/HttpClientFactoryController.cs
@@ -1,4 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ClientHttp.NET.Configurations;
+using ClientHttp.NET.Response;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using static ClientHttp.NET.Configurations.ApiConfig;
 
 namespace ClientHttp.NET.Controllers
 {
@@ -6,6 +15,29 @@ namespace ClientHttp.NET.Controllers
     [ApiController]
     public class HttpClientFactoryController : ControllerBase
     {
+        private IHttpClientFactory _httpClientFactory;
 
+        public HttpClientFactoryController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        // Get <Setor>
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            var url = $"{ApiConfig.UrlBase}{ApiConfig._Urls.Setor.Get}";
+
+            HttpClient client = _httpClientFactory.CreateClient();
+
+            var ret = await client.GetAsync(url);
+
+            if (ret.StatusCode != HttpStatusCode.OK)
+                throw new InvalidOperationException("Código de Status Http 200 esperado!");
+
+            var result = await ret.Content.ReadFromJsonAsync<ResponseBase<List<Setor>>>();
+
+            return Ok(result);
+        }
     }
 }
