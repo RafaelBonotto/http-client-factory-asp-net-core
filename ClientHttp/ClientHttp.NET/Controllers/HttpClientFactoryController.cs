220cb92bab1bc85447c129d76768012096295a09
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-11-30 16:20:03 -0300

    Post com HttpClientFactory

--- ClientHttp/ClientHttp.NET/Controllers/HttpClientFactoryController.cs
+++ ClientHttp/ClientHttp.NET/Controllers/HttpClientFactoryController.cs
@@ -1,11 +1,14 @@
 ﻿using ClientHttp.NET.Configurations;
+using ClientHttp.NET.Request.Setor;
 using ClientHttp.NET.Response;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Text;
 using System.Threading.Tasks;
 using static ClientHttp.NET.Configurations.ApiConfig;
 
@@ -38,6 +41,33 @@ namespace ClientHttp.NET.Controllers
             var result = await ret.Content.ReadFromJsonAsync<ResponseBase<List<Setor>>>();
 
             return Ok(result);
+        }
+
+        // Post <Setor>
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(SetorRequest request)
+        {
+            try
+            {
+                var url = $"{ApiConfig.UrlBase}{ApiConfig._Urls.Setor.Post}";
+
+                var content = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+
+                HttpClient client = _httpClientFactory.CreateClient();
+
+                var ret = await client.PostAsync(url, content);
+
+                if (ret.StatusCode != HttpStatusCode.OK)
+                    throw new InvalidOperationException("Código de Status Http 200 esperado!");
+
+                var result = await ret.Content.ReadFromJsonAsync<ResponseBase<Setor>>();
+
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
         }
     }
 }
