b98d08b3d9437587a36424be80a149af09cd8f0b
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-11-30 16:07:39 -0300

    Create HttpClientFactoryController

---
+++ ClientHttp/ClientHttp.NET/Controllers/HttpClientFactoryController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClientHttp.NET.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HttpClientFactoryController : ControllerBase
+    {
+
+    }
+}
